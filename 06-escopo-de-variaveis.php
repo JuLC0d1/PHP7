5f63cb9e9af314b99f7b57fcbd120b5e6a8e8adb
Juliana <jd391829@gmail.com>
2023-03-10 20:58:24 -0300

    a

--- 06-escopo-de-variaveis.php
+++ 06-escopo-de-variaveis.php
@@ -1,3 +1,35 @@
 <?php
+
+//  ESCOPO GLOBAL
+/*
 $nome = "Tayler Kuoti";
+
+function exibeNome(){
+    echo $nome;
+}
+
+exibeNome();
+*/
+
+$nome = "Tayler Kuoti";
+
+function exibeNome(){
+    global $nome;
+    echo $nome;
+}
+exibeNome();
+
+echo "<hr>";
+
+////////////////////
+
+function exibeCidade(){
+    global $cidade;
+    $cidade = "Roacam";
+}
+
+exibeNome();
+echo $cidade;
+
+echo "<hr>";
 
